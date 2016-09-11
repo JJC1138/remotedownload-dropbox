0d552cf285d934406d83113ec66b70f72ee3f4ea
Jon Colverson <github@vcxz.co.uk>
2016-09-11 21:53:09 +0100

    Added basic test of the Dropbox file uploading API

--- remotedownloaddropbox/__main__.py
+++ remotedownloaddropbox/__main__.py
@@ -13,6 +13,14 @@ def main():
 
     config = configparser.ConfigParser()
     if len(config.read(config_filename)) == 0:
-        raise Exception("Configuration file couldn't be opened: %s" % config_filename)
+        raise Exception("Configuration file couldn't be read: %s" % config_filename)
 
     dbx = dropbox.Dropbox(config['default']['access token'])
+
+    upload_cursor = dropbox.files.UploadSessionCursor(dbx.files_upload_session_start(b'').session_id, 0)
+    def add_bytes(bytes_to_upload):
+        dbx.files_upload_session_append_v2(bytes_to_upload, upload_cursor)
+        upload_cursor.offset += len(bytes_to_upload)
+    add_bytes('hello'.encode('utf-8'))
+    add_bytes(' once more'.encode('utf-8'))
+    dbx.files_upload_session_finish(b'', upload_cursor, dropbox.files.CommitInfo('/hi.txt', dropbox.files.WriteMode.add, autorename=True))
