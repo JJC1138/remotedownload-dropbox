16c0c63d24a7d8b5db0f7944ea0f9910626c0421
Jon Colverson <github@vcxz.co.uk>
2016-09-11 21:16:27 +0100

    Added reading of Dropbox access token from a config file

--- remotedownloaddropbox/__main__.py
+++ remotedownloaddropbox/__main__.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
+import configparser
+import os
+
+import dropbox
+
 def main():
-    pass
+    config_dir = os.path.join(
+        os.environ.get('XDG_CONFIG_HOME') or os.path.join(os.path.expanduser('~'), '.config'),
+        'remotedownload-dropbox')
+    config_filename = os.path.join(config_dir, 'config.ini')
+
+    config = configparser.ConfigParser()
+    if len(config.read(config_filename)) == 0:
+        raise Exception("Configuration file couldn't be opened: %s" % config_filename)
+
+    dbx = dropbox.Dropbox(config['default']['access token'])
