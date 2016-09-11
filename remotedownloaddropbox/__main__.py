f8589a01b297faa550d0256dc5ab3e61afad15c8
Jon Colverson <github@vcxz.co.uk>
2016-09-11 22:40:58 +0100

    Implemented glue between remotedownload and Dropbox

--- remotedownloaddropbox/__main__.py
+++ remotedownloaddropbox/__main__.py
@@ -2,8 +2,11 @@
 
 import configparser
 import os
+import sys
 
 import dropbox
+
+import remotedownload
 
 def main():
     config_dir = os.path.join(
@@ -15,12 +18,27 @@ def main():
     if len(config.read(config_filename)) == 0:
         raise Exception("Configuration file couldn't be read: %s" % config_filename)
 
+    downloader = remotedownload.Downloader(sys.stdin.buffer.read())
+
     dbx = dropbox.Dropbox(config['default']['access token'])
 
-    upload_cursor = dropbox.files.UploadSessionCursor(dbx.files_upload_session_start(b'').session_id, 0)
-    def add_bytes(bytes_to_upload):
-        dbx.files_upload_session_append_v2(bytes_to_upload, upload_cursor)
-        upload_cursor.offset += len(bytes_to_upload)
-    add_bytes('hello'.encode('utf-8'))
-    add_bytes(' once more'.encode('utf-8'))
-    dbx.files_upload_session_finish(b'', upload_cursor, dropbox.files.CommitInfo('/hi.txt', dropbox.files.WriteMode.add, autorename=True))
+    class DropboxFileUpload:
+        """A file-like object for uploading to a Dropbox upload session"""
+
+        def __init__(self):
+            self._cursor = dropbox.files.UploadSessionCursor(dbx.files_upload_session_start(b'').session_id, 0)
+
+        def write(self, data):
+            # The Dropbox API will happily take a string but we need to know the length of its byte representation to advance the cursor so just convert it ourselves:
+            data_bytes = data.encode('utf-8') if type(data) is str else data
+            dbx.files_upload_session_append_v2(data_bytes, self._cursor)
+            self._cursor.offset += len(data_bytes)
+
+        def commit(self, filename):
+            dbx.files_upload_session_finish(b'', self._cursor,
+                dropbox.files.CommitInfo('/%s' % filename, dropbox.files.WriteMode.add, autorename=True))
+
+    for url in downloader.urls:
+        upload = DropboxFileUpload()
+        filename = downloader.get(url, upload, chunk_size=2 ** 20)
+        upload.commit(filename)
